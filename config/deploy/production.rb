2890cd48047f176aa807f294542e2ab0effd03f8
tomoyahiraki <tomoyahiraki0616@gamil.com>
2019-01-28 17:26:30 +0900

    Add

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server 'http://54.250.217.113', user: 'ec2-user', roles: %w{app db web}
+server '54.250.217.113', user: 'ec2-user', roles: %w{app db web}
 
 
 # role-based syntax
